0bec2cca4fbc536f45583d7ac840b3d6cbd55aac
master <alxx85>
2018-10-08 00:44:43 +0700

    VK groups

--- diplom.py
+++ diplom.py
@@ -6,15 +6,12 @@ import time
 
 class User:
     """ Класс для экземпляров описывающих пользователей VK и реализующий поиск групп,
-        в которых состоит пользователь, но не состоит никто из его друзей
+        в которых состоит пользователь.
         Экземпляр класса создается на основе идентификатора пользователя VK"""
 
     def __init__(self, user_id):
         self.token = constants.TOKEN
         self.user_id = user_id
-
-    def __str__(self):
-        return str(self.get_info())
 
     def get_params(self):
         return {
@@ -22,24 +19,18 @@ class User:
             'v': '5.85'
             }
 
-    def get_requests(self):
-        print('.')
-        time.sleep(0.4)
-
-    def get_info(self):
+    def get_info(self):  #Получение информации о пользователе
         params = self.get_params()
         params['user_ids'] = self.user_id
-        self.get_requests()
         response = requests.get('https://api.vk.com/method/users.get', params).json()
         print(response['response'][0]['first_name'], response['response'][0]['last_name'])
         if response['response'][0]['id'] != self.user_id:
             self.user_id = int(response['response'][0]['id'])
 
-    def friends_user(self):
+    def friends_user(self):  #Формирование списка друзей
         params = self.get_params()
         params['user_id'] = self.user_id
         friends = list()
-        self.get_requests()
         response = requests.get('https://api.vk.com/method/friends.get', params).json()
         if 'response' in response:
             lst_friends = response['response']['items']
@@ -48,35 +39,24 @@ class User:
                     friends.append(User(friend))
         return friends
 
-    def groups_user(self):
+    def groups_user(self):  #Формирование списка групп пользователя
         params = self.get_params()
         params['user_id'] = self.user_id
-        self.get_requests()
-        response = requests.get('https://api.vk.com/method/groups.get', params).json()
-        if 'response' in response:
+        try:
+            response = requests.get('https://api.vk.com/method/groups.get', params).json()
+            #    if 'response' in response:
             lst_groups = response['response']['items']
-        return lst_groups
+            return lst_groups
+        except:
+            print(f'Пользователь с ID{self.user_id} недоступен...')
+            return list()
 
-    def groups_none_friends(self, list_groups):
-        params = self.get_params()
-        params['filter'] = 'friends'
-        list_groups_none_friends = list()
-        for group in list_groups:
-            params['group_id'] = group
-            self.get_requests()
-            response = requests.get('https://api.vk.com/method/groups.getMembers', params).json()
-            if 'response' in response:
-                if response['response']['count'] == 0:
-                    list_groups_none_friends.append(group)
-        return list_groups_none_friends
-
-    def get_group_info(self, groups_lst):
+    def get_group_info(self, groups_lst):  #Формирование словаря с описанием групп в токорых не состоит никто из друзей
         group_info = list()
         params = self.get_params()
         params['fields'] = 'members_count'
         for group_id in groups_lst:
             params['group_ids'] = group_id
-            self.get_requests()
             response = requests.get('https://api.vk.com/method/groups.getById', params).json()
             if 'response' in response:
                 group_inf = dict()
@@ -87,12 +67,32 @@ class User:
         return group_info
 
 
+def get_requests(max_quantity = 1, current_numb = 1):  #Отображение выполняемого запроса и общее их колличество
+    print(f'Выполняется запрос №{current_numb} из {max_quantity}.')
+    if current_numb >= max_quantity:
+        current_numb = 0
+    time.sleep(0.34)
+    current_numb += 1
+
+#friends_dict = dict()
+numb_request = 1
 user1 = User(constants.USER_ID)
+get_requests()
 user1.get_info()
-#friend_list = user1.friends_user()
-#print('Колличество друзей: ', len(friend_list))
-groups_list = user1.groups_user()
-groups = user1.groups_none_friends(groups_list)
+get_requests()
+groups = set(user1.groups_user())
+print('Колличество групп: ', len(groups_set))
+get_requests()
+friend_list = user1.friends_user()
+print('Колличество друзей: ', len(friend_list))
+numb_request = 1
+for friend in friend_list:
+    get_requests(len(friend_list), numb_request)
+    groups_friend = list(friend.groups_user())
+    if len(groups_friend) != 0:
+        #friends_dict[friend] = groups_friend
+        groups -= set(groups_friend)
+    numb_request += 1
 if len(groups) > 0:
     data = user1.get_group_info(groups)
 with open('groups.json', 'w', encoding = 'utf8') as f:
